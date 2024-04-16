0431ac0a0c1d5a03d7a8754611bcff89092adf11
Cecilia Suarez <cecisuarez08@gmail.com>
2024-04-16 16:00:40 -0300

    Trabajando en .dark

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import { useContextGlobal } from '../Components/utils/global.context'
 import cardGridStyle from '../Styles/Cards.module.css'
 
 
+
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
@@ -11,7 +12,7 @@ const Home = () => {
   const { list } = useContextGlobal()
 
   return (
-      <div>
+      <div className={cardGridStyle.containerCard}>
         <h1>Home</h1>
         <div className={cardGridStyle.cardGrid}>
           {list.map(dentist => <Card  dentist={dentist} key={dentist.id}/>)}
